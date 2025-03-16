96d8cfdbb0127a1f14dd29138ba2108e575e8371
fesuard <fesuard@gmail.com>
2025-03-16 14:58:45 +0200

    Timer project finished

---
+++ StopWatch/index.js
@@ -0,0 +1,52 @@
+const display = document.getElementById("display");
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+
+    if(!isRunning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(update, 10);
+        isRunning = true;
+    }
+}
+
+function stop(){
+    
+    if(isRunning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime; // not necessarily needed unless you want to log somewhere the time passed between
+                                              // each start-stop cycle
+        isRunning = false;
+    }
+    
+}
+
+function reset(){
+
+    clearInterval(timer);
+    startTime = 0;
+    elapsedTime = 0;
+    isRunning = false;
+    display.textContent = "00:00:00:00";
+}
+
+function update(){
+
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000 % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10); // we divide this by 10 since we will just take the first 2 digits
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, "0");
+
+    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
